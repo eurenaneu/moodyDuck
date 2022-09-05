cee061da689ba53ac20e1aca290c76a67969237e
renan_p_machado <itzchrooma@gmail.com>
2022-09-05 09:51:59 -0300

    erro depressão

--- app/src/main/java/com/example/moodyduck/Stats.java
+++ app/src/main/java/com/example/moodyduck/Stats.java
@@ -1,5 +1,6 @@
 package com.example.moodyduck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -13,13 +14,22 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Stats extends AppCompatActivity {
     LineChart lc;
     ArrayList lineArrayList;
+    int bd = 0;
+    DatabaseReference ref;
+    Calendar c = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +38,7 @@ public class Stats extends AppCompatActivity {
         setContentView(R.layout.activity_stats);
         lc = findViewById(R.id.linechart);
         lineArrayList = new ArrayList();
-        lineArrayList.add(new BarEntry(1f, 1));
-        lineArrayList.add(new BarEntry(2f, 2));
-        lineArrayList.add(new BarEntry(3f, 4));
-        lineArrayList.add(new BarEntry(4f, 8));
-        lineArrayList.add(new BarEntry(5f, 16));
-        lineArrayList.add(new BarEntry(6f, 32));
-        lineArrayList.add(new BarEntry(7f, 64));
-        lineArrayList.add(new BarEntry(8f, 128));
-        lineArrayList.add(new BarEntry(9f, 256));
-        lineArrayList.add(new BarEntry(10f, 512));
-        lineArrayList.add(new BarEntry(11f, 1024));
-        lineArrayList.add(new BarEntry(12f, 2048));
-        lineArrayList.add(new BarEntry(13f, 4096));
+        getData();
         LineDataSet lineDataset = new LineDataSet(lineArrayList, "");
         lineDataset.setValueTextColor(ContextCompat.getColor(this, R.color.amalero));
         lineDataset.setValueTextSize(16);
@@ -64,5 +62,38 @@ public class Stats extends AppCompatActivity {
         lc.setDoubleTapToZoomEnabled(false);
         lc.setBorderColor(Color.WHITE);
         lc.getLegend().setEnabled(false);
+    }
+
+    public void getData(){
+        String[] nomeMes = new String[]{"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+        ref = FirebaseDatabase.getInstance().getReference().child(nomeMes[c.get(Calendar.MONTH)]+"-"+c.get(Calendar.YEAR));
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    bd = (int) dataSnapshot.getValue();
+                    lineArrayList.add(new BarEntry(1f, bd));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        lineArrayList.add(new BarEntry(1f, bd));
+        lineArrayList.add(new BarEntry(2f, 2));
+        lineArrayList.add(new BarEntry(3f, 4));
+        lineArrayList.add(new BarEntry(4f, 8));
+        lineArrayList.add(new BarEntry(5f, 16));
+        lineArrayList.add(new BarEntry(6f, 32));
+        lineArrayList.add(new BarEntry(7f, 64));
+        lineArrayList.add(new BarEntry(8f, 128));
+        lineArrayList.add(new BarEntry(9f, 256));
+        lineArrayList.add(new BarEntry(10f, 512));
+        lineArrayList.add(new BarEntry(11f, 1024));
+        lineArrayList.add(new BarEntry(12f, 2048));
+        lineArrayList.add(new BarEntry(13f, 4096));
     }
 }
