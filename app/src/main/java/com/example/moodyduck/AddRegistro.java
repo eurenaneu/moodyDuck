c5f388f8e1e34b96d7a9c978ca4da6045c8fe87d
renan_p_machado <itzchrooma@gmail.com>
2022-09-15 09:51:41 -0300

    Ajustado algumas configurações de horário

--- app/src/main/java/com/example/moodyduck/AddRegistro.java
+++ app/src/main/java/com/example/moodyduck/AddRegistro.java
@@ -3,15 +3,20 @@ package com.example.moodyduck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
 import java.util.Calendar;
@@ -20,10 +25,12 @@ import java.util.Date;
 public class AddRegistro extends AppCompatActivity {
     DatePickerDialog.OnDateSetListener setListener;
     Calendar c = Calendar.getInstance();
-    int dia, mes, ano;
-    String data;
+    int dia, mes, ano, hora, min;
+    String data, horario;
     TextView tvData;
     Button bVoltar;
+    SimpleDateFormat f24 = new SimpleDateFormat("HH:mm");
+    String[] nomeMes = {"janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"};/* = getResources().getStringArray(R.array.nomeMes);*/
     Date dataHoraAtual = new Date();
     static String tData;
 
@@ -37,35 +44,41 @@ public class AddRegistro extends AppCompatActivity {
         mes = c.get(Calendar.MONTH);
         ano = c.get(Calendar.YEAR);
         c.set(dia, mes, ano);
-        String hora = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
-        String[] nomeMes = {"janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"};
+        horario = f24.format(dataHoraAtual);
+        preEscolha();
+        setarData();
+        bVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 
+    public void preEscolha(){
         //pre-escolha
         if(tData.equals("Ontem")){
-            data = tData+", "+(dia-1)+" de "+nomeMes[mes]+", "+ hora;
+            data = tData+", "+(dia-1)+" de "+nomeMes[mes]+", "+ horario;
             tvData.setText(data);
         }
 
         else if(tData.equals("Hoje")){
-            data = tData+", "+dia+" de "+nomeMes[mes]+", "+ hora;
+            data = tData+", "+dia+" de "+nomeMes[mes]+", "+ horario;
             tvData.setText(data);
         }
 
         else if(tData.equals("Outro dia")){
-            data = tData+", "+(dia+1)+" de "+nomeMes[mes]+", "+ hora;
+            data = tData+", "+(dia-3)+" de "+nomeMes[mes]+", "+ horario;
             tvData.setText(data);
         }
+    }
 
-        bVoltar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(AddRegistro.this, AddRegistro.class));
-            }
-        });
+    public void setarData(){
         tvData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(AddRegistro.this, android.R.style.Theme_Holo_Dialog_MinWidth,setListener,ano,mes,dia);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
                 datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 datePickerDialog.show();
             }
@@ -86,10 +99,45 @@ public class AddRegistro extends AppCompatActivity {
                     else{
                         tData = "Outro dia";
                     }
+                } else {
+                    tData = "Outro dia";
                 }
-                String data = tData+", "+dayOfMonth+" de "+nomeMes[month]+", ";
+
+                if(year != ano){
+                    data = tData+", "+dayOfMonth+" de "+nomeMes[month]+" de "+year;
+                } else {
+                    data = tData + ", " + dayOfMonth + " de " + nomeMes[month];
+                }
                 tvData.setText(data);
+                setarHorario();
             }
         };
+    }
+
+    public void setarHorario(){
+            TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    hora = hourOfDay;
+                    min = minute;
+                    horario = hourOfDay+":"+minute;
+                    try {
+                        dataHoraAtual = f24.parse(horario);
+                        tvData.setText(data+=", "+f24.format(dataHoraAtual));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            TimePickerDialog timePickerDialog = new TimePickerDialog(AddRegistro.this, android.R.style.Theme_Holo_Dialog_MinWidth, timePickerListener, 12, 0, true);
+            timePickerDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    tvData.setText(data+=", "+horario);
+                }
+            });
+            timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            timePickerDialog.updateTime(hora, min);
+            timePickerDialog.show();
     }
 }
