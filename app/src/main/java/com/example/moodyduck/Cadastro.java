3ffccf710fce3532a401bddccb5f5eb581755433
renan_p_machado <itzchrooma@gmail.com>
2022-09-27 12:26:11 -0300

    Firebase Auth implementado

--- app/src/main/java/com/example/moodyduck/Cadastro.java
+++ app/src/main/java/com/example/moodyduck/Cadastro.java
@@ -1,5 +1,6 @@
 package com.example.moodyduck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,17 +9,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Cadastro extends AppCompatActivity {
     TextView aviso;
-    EditText senha, confirma;
+    EditText email, senha, confirma;
     FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         aviso = findViewById(R.id.avisoSenhas);
+        email = findViewById(R.id.campoEmail);
         senha = findViewById(R.id.campoSenha);
         confirma = findViewById(R.id.campoConfirma);
         fab = findViewById(R.id.fab);
@@ -27,5 +33,21 @@ public class Cadastro extends AppCompatActivity {
 
     public void irHome(View v){
         startActivity(new Intent(this, Home.class));
+    }
+
+    public void cadastrarUser(View v){
+        String e = email.getText().toString();
+        String s = senha.getText().toString();
+        String cs = confirma.getText().toString();
+        if(s != cs) {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(e, s).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        //continuar depois
+                    }
+                }
+            });
+        }
     }
 }
