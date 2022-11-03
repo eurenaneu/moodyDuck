b19e8612c965bce0ee79013c9501642857616228
renan_p_machado <itzchrooma@gmail.com>
2022-11-03 10:45:53 -0300

    Começar adaptador do RecyclerView (Objetivos na tela de Estatísticas)

--- app/src/main/java/com/example/moodyduck/Adaptador.java
+++ app/src/main/java/com/example/moodyduck/Adaptador.java
@@ -1,4 +1,53 @@
 package com.example.moodyduck;
 
-public class Adaptador {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.MyViewHolder> {
+    Context c;
+    ArrayList<Resultados> resultados;
+
+    public Adaptador(Context c, ArrayList<Resultados> resultados) {
+        this.c = c;
+        this.resultados = resultados;
+    }
+
+    @NonNull
+    @Override
+    public Adaptador.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(c);
+        View v = inflater.inflate(R.layout.layout_rv_objetivos, parent, false);
+        return new Adaptador.MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adaptador.MyViewHolder holder, int position) {
+        Resultados r = resultados.get(position);
+        holder.t.setText(r.data);
+        holder.a.setText(r.area);
+        holder.p.setText(r.profissao);
+    }
+
+    @Override
+    public int getItemCount() {
+        return resultados.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView t, p, a;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            t = itemView.findViewById(R.id.dataCard);
+            p = itemView.findViewById(R.id.profCard);
+            a = itemView.findViewById(R.id.areaCard);
+        }
+    }
 }
