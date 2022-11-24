beea3187bd2d2d7ebdf82e88e3b1a55b10990440
chroma <itzchrooma@gmail.com>
2022-11-24 20:16:30 -0300

    Continuar objetivos em Stats

--- app/src/main/java/com/example/moodyduck/Config.java
+++ app/src/main/java/com/example/moodyduck/Config.java
@@ -2,20 +2,25 @@ package com.example.moodyduck;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Config extends AppCompatActivity {
     TextView txtInfo;
     AlertDialog alerta;
+    SwitchCompat switchLembretes;
     Button bRelatorioM, bAlterarSenha, bDeslogar;
 
     @Override
@@ -27,6 +32,7 @@ public class Config extends AppCompatActivity {
         bRelatorioM = findViewById(R.id.bRelatorioM);
         bAlterarSenha = findViewById(R.id.bAlterarSenha);
         bDeslogar = findViewById(R.id.bDeslogar);
+        switchLembretes = findViewById(R.id.switchLembretes);
 
         // setar info
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -86,5 +92,20 @@ public class Config extends AppCompatActivity {
     public void onBackPressed(){
         startActivity(new Intent(getApplicationContext(), Home.class));
         this.finish();
+    }
+
+    public void ativarLembretes(View view){
+        switchLembretes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    Snackbar snackbar = Snackbar.make(view, "Lembretes ativados", 2000);
+                    snackbar.show();
+                } else {
+                    Snackbar snackbar = Snackbar.make(view, "Lembretes desativados", 2000);
+                    snackbar.show();
+                }
+            }
+        });
     }
 }
